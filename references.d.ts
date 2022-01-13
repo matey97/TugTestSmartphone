56c25c5922501558f9938d57ec3be695b9401bd3
Miguel <al341802@uji.es>
2022-01-13 12:17:29 +0100

    Initial setup

--- references.d.ts
+++ references.d.ts
@@ -1,1 +1,5 @@
 /// <reference path="./node_modules/@nativescript/types/index.d.ts" />
+
+/// <reference path="./typings/android/tflite.d.ts" />
+/// <reference path="./typings/android/tflite-metadata.d.ts" />
+/// <reference path="./typings/android/firebase-ml-downloader.d.ts" />
