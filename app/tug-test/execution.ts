bfe970384564a23dfccc5f6432f7036c787c4c39
Miguel <al341802@uji.es>
2022-01-18 17:11:58 +0100

    Improve duration subphases algorithm

--- app/tug-test/execution.ts
+++ app/tug-test/execution.ts
@@ -3,14 +3,15 @@ import { Activity, fromString } from "~/tug-test/activities";
 import { ActivityResult, TugResult } from "~/tug-test/result";
 
 const EXECUTION_SEQUENCE = [
+  Activity.SIT,
   Activity.STANDING_UP,
   Activity.WALKING,
   Activity.TURNING,
   Activity.WALKING,
   Activity.TURNING,
   Activity.SITTING,
+  Activity.SIT,
 ];
-const LAG_AND_LEAD = 2;
 
 export class TugExecution {
 
@@ -59,103 +60,61 @@ export class TugExecution {
   }
 
   computeResults(): TugResult {
-    let results: ActivityResult[] = [];
-    let iActivity = 0;
-    let tugStart, tugEnd;
-    let start = 0, end = 0;
-    for (let i = LAG_AND_LEAD; i <= this.recognitionResults.length - LAG_AND_LEAD; i++) {
-      const recognitionResult = this.recognitionResults[i];
-      if (
-        this.isUnexpectedActivity(
-          fromString(recognitionResult.inference.class),
-          this.expectedActivities(iActivity)
-        )
-      )
+    const changes = this.computeChanges();
+
+    const results: ActivityResult[] = [];
+    for (let i = 0; i < changes.length - 1; i++) {
+      const { name, start } = changes[i];
+      const end = changes[i+1].start;
+      results.push({
+        name: name,
+        start: start,
+        end: end,
+        duration: this.computeMillisecondsBetween(start, end),
+      })
+    }
+
+    const globalStart = results[0].start;
+    const globalEnd = results[results.length - 1].end;
+
+    return this.buildTugResult(this.computeMillisecondsBetween(globalStart, globalEnd), results);
+  }
+
+
+  private computeChanges(): any[] {
+    let iCurrentActivity = 0;
+
+    const changes = [];
+    for (let i = 0; i < this.recognitionResults.length - 1; i++) {
+      const result = this.recognitionResults[i];
+      const after = this.recognitionResults.slice(i, i + 3);
+
+      if (result.inference.class === EXECUTION_SEQUENCE[iCurrentActivity] && result.inference.class === this.recognitionResults[i+1].inference.class)
         continue;
 
-      if (this.isStarting(i) && this.areEqual(recognitionResult.inference.class, EXECUTION_SEQUENCE[iActivity])) {
-        if (iActivity === 0) {
-          tugStart = recognitionResult.timestampStart;
-          console.log(`[START] --> ${tugStart}`);
-        }
-        start = i;
-      }
+      const allAfterDifferentThanCurrent = after.every((elem) => elem.inference.class !== EXECUTION_SEQUENCE[iCurrentActivity]);
+      const someAfterEqualThanNext = after.some((elem) => elem.inference.class === EXECUTION_SEQUENCE[iCurrentActivity + 1]);
 
-      if (this.isEnding(i)) {
-        if (iActivity === EXECUTION_SEQUENCE.length - 1) {
-          tugEnd = recognitionResult.timestampEnd;
-          console.log(`[END] --> ${tugEnd}`);
-        }
-        end = i;
-        const msDuration = this.computeMillisecondsBetween(start, end - 1); // Por solapamiento de ventanas
-        results.push({
-          name: EXECUTION_SEQUENCE[iActivity],
-          start: start,
-          end: end,
-          duration: msDuration,
-        })
-        iActivity += 1;
+      if (allAfterDifferentThanCurrent && someAfterEqualThanNext) {
+        iCurrentActivity++;
+        changes.push({
+          name: EXECUTION_SEQUENCE[iCurrentActivity],
+          start: i,
+        });
       }
     }
 
-    return this.buildTugResult(tugEnd - tugStart, results);
-  }
-
-  private isUnexpectedActivity(current: Activity, expected: Activity[]): boolean {
-    return expected.indexOf(current) === -1;
-  }
-
-  private expectedActivities(current: number): Activity[] {
-    const expected = [EXECUTION_SEQUENCE[current]]
-
-    const next = current + 1;
-    if (next < EXECUTION_SEQUENCE.length)
-      expected.push(EXECUTION_SEQUENCE[next]);
-
-    return expected;
-  }
-
-  private isStarting(current: number) {
-    return this.backAndForth(
-      current,
-      this.areDifferent,
-      this.areEqual
-    );
-  }
-
-  private isEnding(current: number) {
-    return this.backAndForth(
-      current,
-      this.areEqual,
-      this.areDifferent
-    );
-  }
-
-  private areEqual(a, b) {
-    return a === b;
-  }
-
-  private areDifferent(a, b) {
-    return a !== b
-  }
-
-  private backAndForth(current, fPrev, fNext) {
-    const start = current - LAG_AND_LEAD;
-    const end = current + LAG_AND_LEAD;
-
-    const previous = this.recognitionResults
-      .slice(start, current)
-      .every(value => fPrev(value.inference.class, this.recognitionResults[current].inference.class));
-
-    const next = this.recognitionResults
-      .slice(current + 1, end)
-      .every(value => fNext(value.inference.class, this.recognitionResults[current].inference.class));
-
-    return previous && next;
+    return changes;
   }
 
   private computeMillisecondsBetween(start: number, end: number): number {
-    return this.recognitionResults[end].timestampEnd - this.recognitionResults[start].timestampStart;
+    const endResult = this.recognitionResults[end];
+    const startResult = this.recognitionResults[start];
+    return this.meanTime(endResult) - this.meanTime(startResult);
+  }
+
+  private meanTime(result: RecognitionResult): number {
+    return Math.round((result.timestampEnd + result.timestampStart) / 2);
   }
 
   private buildTugResult(totalDuration: number, results: ActivityResult[]): TugResult {
