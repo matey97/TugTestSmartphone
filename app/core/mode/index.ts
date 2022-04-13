9abcffd346e873127334e215985f3e0bb170e1a0
Miguel Matey Sanz <al341802@uji.es>
2022-04-13 09:36:55 +0200

    Add a way to change the sensing source

--- app/core/mode/index.ts
+++ app/core/mode/index.ts
@@ -1,10 +1,16 @@
 import { setNumber, getNumber, flush } from "@nativescript/core/application-settings"
 
 const APP_MODE_KEY = "app_mode";
+const SENSING_DATA_SOURCE_KEY = "sensing_data_source";
 
 export enum ApplicationMode {
   INFERENCE,
   DATA_COLLECTION,
+}
+
+export enum SensingDataSource {
+  LOCAL_DEVICE,
+  PAIRED_DEVICE,
 }
 
 export function setApplicationMode(mode: ApplicationMode) {
@@ -14,4 +20,13 @@ export function setApplicationMode(mode: ApplicationMode) {
 
 export function getApplicationMode(): ApplicationMode {
   return getNumber(APP_MODE_KEY);
+}
+
+export function setSensingDataSource(source: SensingDataSource) {
+  setNumber(SENSING_DATA_SOURCE_KEY, source);
+  flush();
+}
+
+export function getSensingDataSource(): SensingDataSource {
+  return getNumber(SENSING_DATA_SOURCE_KEY);
 }
