dd5499c64556fddac6cd321613d53b09b0de879a
Miguel Matey Sanz <al341802@uji.es>
2022-04-26 17:11:44 +0200

    Add specific recognizers

---
+++ app/core/recognition/recognizer/mlp/mlp-recognizer.ts
@@ -0,0 +1,24 @@
+import { AbstractRecognizer} from "~/core/recognition/recognizer/abstract-recognizer";
+import { ModelType, SensingDataSource } from "~/core/mode";
+import { extractFeaturesFrom, Samples } from "~/core/feature-extraction";
+import ByteBuffer = java.nio.ByteBuffer;
+
+export class MLPRecognizer extends AbstractRecognizer {
+
+  constructor(dataSource: SensingDataSource) {
+    super(dataSource, ModelType.MLP);
+  }
+
+  protected createInputBuffer(samples: Samples): java.nio.ByteBuffer {
+    const extractedFeatures = extractFeaturesFrom(samples);
+    const features = extractedFeatures.features;
+
+    const input: ByteBuffer = this.createBuffer(features.length, java.lang.Float.SIZE);
+
+    for (let feature of features) {
+      input.putFloat(feature);
+    }
+
+    return input;
+  }
+}
