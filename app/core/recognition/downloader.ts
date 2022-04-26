e07d5c1392673da1367a1d6013828aac5e1025b2
Miguel Matey Sanz <al341802@uji.es>
2022-04-26 17:12:10 +0200

    Update downloader with model type

--- app/core/recognition/downloader.ts
+++ app/core/recognition/downloader.ts
@@ -1,4 +1,4 @@
-import { SensingDataSource } from "~/core/mode";
+import { ModelType, SensingDataSource } from "~/core/mode";
 import CustomModelDownloadConditions = com.google.firebase.ml.modeldownloader.CustomModelDownloadConditions;
 import CustomModel = com.google.firebase.ml.modeldownloader.CustomModel;
 import FirebaseModelDownloader = com.google.firebase.ml.modeldownloader.FirebaseModelDownloader;
@@ -7,15 +7,24 @@ import OnSuccessListener = com.google.android.gms.tasks.OnSuccessListener;
 import OnFailureListener = com.google.android.gms.tasks.OnFailureListener;
 
 const modelNameForDataSource = new Map([
-  [SensingDataSource.LOCAL_DEVICE, "smartphone-model"],
-  [SensingDataSource.PAIRED_DEVICE, "tug-test-full"]
+  [SensingDataSource.LOCAL_DEVICE, new Map([
+    [ModelType.MLP, "smartphone-model"],
+    [ModelType.CNN, "cnn-smartphone"]
+  ])],
+  [SensingDataSource.PAIRED_DEVICE, new Map([
+    [ModelType.MLP, "tug-test-full"],
+    [ModelType.CNN, "cnn-smartwatch"]
+  ])]
 ]);
 
 export class ModelDownloader {
 
   private modelFilePath: string;
 
-  constructor(private dataSource: SensingDataSource) {
+  constructor(
+    private dataSource: SensingDataSource,
+    private modelType: ModelType
+  ) {
   }
 
   async getModelFilePath(): Promise<string> {
@@ -34,7 +43,7 @@ export class ModelDownloader {
   }
 
   private startDownloadTask(conditions: CustomModelDownloadConditions): Promise<CustomModel> {
-    const modelName = modelNameForDataSource.get(this.dataSource);
+    const modelName = modelNameForDataSource.get(this.dataSource).get(this.modelType);
     const downloadTask = FirebaseModelDownloader.getInstance()
       .getModel(modelName, DownloadType.LATEST_MODEL, conditions)
 
