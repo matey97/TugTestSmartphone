4f7a4b89f22af70e5753a6d0a81d6780da4f8b13
Miguel Matey Sanz <al341802@uji.es>
2022-05-02 16:22:36 +0200

    Update references

--- references.d.ts
+++ references.d.ts
@@ -2,5 +2,6 @@
 /// <reference path="./node_modules/@nativescript/types-ios/index.d.ts" />
 
 /// <reference path="./typings/android/tflite.d.ts" />
+/// <reference path="./typings/android/tflite-gpu.d.ts" />
 /// <reference path="./typings/android/tflite-metadata.d.ts" />
 /// <reference path="./typings/android/firebase-ml-downloader.d.ts" />
