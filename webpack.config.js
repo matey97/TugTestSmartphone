85a03e0c0c5ed232b1f026482bb212938278b142
Miguel Matey Sanz <al341802@uji.es>
2022-10-06 15:20:04 +0200

    refactor: remove unnecessary components

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,6 @@
 const webpack = require("@nativescript/webpack");
 
 module.exports = (env) => {
-  env.appComponents = (env.appComponents || []).concat(['./app/core/collection/local-collection-service.android']);
 	webpack.init(env);
 
 	// Learn how to customize:
